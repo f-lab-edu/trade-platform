0555c491bbe257259d9b95dd3111daa89c605171
Seungchul Lee <mangfu100@gmail.com>
2022-01-20 21:24:18 +0900

    trade-server에 시작점 Main 함수 추가

---
+++ trade-server/src/main/java/team/comito/trade/server/TradeServerApplication.java
@@ -0,0 +1,11 @@
+package team.comito.trade.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeServerApplication.class,args);
+    }
+}
