8ccdef31e9b65cb85aae25bd90730b0d323af1fe
Seungchul Lee <mangfu100@gmail.com>
2022-01-25 00:46:34 +0900

    ScanBasePackage 추가

--- trade-app/src/main/java/team/comito/trade/app/TradeAppApplication.java
+++ trade-app/src/main/java/team/comito/trade/app/TradeAppApplication.java
@@ -3,7 +3,7 @@ package team.comito.trade.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "team.comito")
 public class TradeAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradeAppApplication.class,args);
