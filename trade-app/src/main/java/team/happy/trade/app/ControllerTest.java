2a229a9e1f77ed1881abf4b2977ac0c597a7b1cf
Seungchul Lee <mangfu100@gmail.com>
2022-01-29 21:05:04 +0900

    linters 추가

--- trade-app/src/main/java/team/happy/trade/app/ControllerTest.java
+++ trade-app/src/main/java/team/happy/trade/app/ControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerTest {
-
     @RequestMapping(path = "/main/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok(HttpStatus.OK);
