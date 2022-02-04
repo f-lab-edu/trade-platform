9426d3336a1e8c3b0b5fe60585e64bc716e02efc
Seungchul Lee <mangfu100@gmail.com>
2022-02-04 22:55:15 +0900

    기존 코드 삭제 커밋

--- trade-user/src/main/java/team/world/trade/user/utils/UserUtils.java
+++
@@ -1,8 +0,0 @@
-package team.world.trade.user.utils;
-
-public class UserUtils {
-
-    public static boolean isSameString(String password, String confirmedPassword) {
-        return password.equals(confirmedPassword) ? true : false;
-    }
-}
