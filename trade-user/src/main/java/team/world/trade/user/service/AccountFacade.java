88c5f871eb30438e448a68a1ad1edd4cc0ceac7c
Seungchul Lee <mangfu100@gmail.com>
2022-02-08 04:07:20 +0900

    AccountFacade에 repository 인터페이스 삭제

--- trade-user/src/main/java/team/world/trade/user/service/AccountFacade.java
+++ trade-user/src/main/java/team/world/trade/user/service/AccountFacade.java
@@ -3,7 +3,6 @@ package team.world.trade.user.service;
 import org.springframework.stereotype.Service;
 import team.world.trade.user.model.dtos.LoginAccountDto;
 import team.world.trade.user.model.dtos.RegisterAccountDto;
-import team.world.trade.user.repository.AccountRepository;
 import team.world.trade.user.response.payload.AccountResponse;
 import team.world.trade.user.service.account.LoginAccountService;
 import team.world.trade.user.service.account.RegisterAccountService;
@@ -13,23 +12,18 @@ public class AccountFacade {
 
     private final LoginAccountService loginAccountService;
     private final RegisterAccountService registerAccountService;
-    private final AccountRepository accountRepository;
 
     public AccountFacade(LoginAccountService loginAccountService,
-                         RegisterAccountService registerAccountService,
-                         AccountRepository accountRepository) {
+                         RegisterAccountService registerAccountService) {
         this.loginAccountService = loginAccountService;
         this.registerAccountService = registerAccountService;
-        this.accountRepository = accountRepository;
     }
 
     public AccountResponse login(LoginAccountDto loginAccountDto) {
-        return loginAccountService.login(loginAccountDto.getUsername(),
-                loginAccountDto.getPassword());
+        return loginAccountService.login(loginAccountDto);
     }
 
     public AccountResponse register(RegisterAccountDto registerAccountDto) {
-        return registerAccountService.register(registerAccountDto.getUsername(),
-                registerAccountDto.getEmail(), registerAccountDto.getPassword());
+        return registerAccountService.register(registerAccountDto);
     }
 }
