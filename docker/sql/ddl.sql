e1bb882c1e427392b2202bc940f8ece0d9ce1020
Seungchul Lee <mangfu100@gmail.com>
2022-02-21 14:22:10 +0900

    trade-user 모듈에 mybatis 적용

--- docker/sql/ddl.sql
+++ docker/sql/ddl.sql
@@ -1,7 +1,7 @@
-create database commerce CHARACTER SET utf8;
+create database mysql CHARACTER SET utf8;
 
 CREATE USER 'sclee'@'localhost' IDENTIFIED BY 'password!';
 CREATE USER 'sclee'@'%' IDENTIFIED BY 'password!';
 
-GRANT ALL PRIVILEGES ON commerce.* TO 'sclee'@'localhost';
-GRANT ALL PRIVILEGES ON commerce.* TO 'sclee'@'%';
+GRANT ALL PRIVILEGES ON mysql.* TO 'sclee'@'localhost';
+GRANT ALL PRIVILEGES ON .* TO 'sclee'@'%';
--- trade-app/src/main/java/team/world/trade/app/TradeAppApplication.java
+++ trade-app/src/main/java/team/world/trade/app/TradeAppApplication.java
@@ -1,8 +1,11 @@
 package team.world.trade.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@MapperScan(basePackages = "team.world")
 @SpringBootApplication(scanBasePackages = "team.world")
 public class TradeAppApplication {
     public static void main(String[] args) {
