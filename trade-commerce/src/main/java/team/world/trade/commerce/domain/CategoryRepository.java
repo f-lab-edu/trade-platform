015c0e51bbfaf65008b81716fb81ba12d195d4be
Seungchul Lee <mangfu100@gmail.com>
2022-02-23 13:49:04 +0900

    애노테이션 제거

--- trade-commerce/src/main/java/team/world/trade/commerce/domain/CategoryRepository.java
+++ trade-commerce/src/main/java/team/world/trade/commerce/domain/CategoryRepository.java
@@ -1,9 +1,7 @@
 package team.world.trade.commerce.domain;
 
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository {
 
     void save(Category category);
--- trade-commerce/src/main/java/team/world/trade/commerce/domain/ProductRepository.java
+++ trade-commerce/src/main/java/team/world/trade/commerce/domain/ProductRepository.java
@@ -1,9 +1,7 @@
 package team.world.trade.commerce.domain;
 
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository {
 
     void save(Product product);
